34c9132d229eba3dcad227750f3a31a2575436bb
hrishikeshmohan23 <57557526+hrishikeshmohan23@users.noreply.github.com>
2020-07-11 00:51:37 +0530

    Add files via upload

--- 1091A.cpp
+++ 1091A.cpp
@@ -1,35 +1,39 @@
-#include <bits/stdc++.h>
-
-#define endl "\n"
-#define ll long long int
-#define vi vector<int>
-#define vll vector<ll>
-#define vvi vector < vi >
-#define pii pair<int,int>
-#define pll pair<long long, long long>
-#define mod 1000000007
-#define inf 1000000000000000001;
-#define all(c) c.begin(),c.end()
-#define mp(x,y) make_pair(x,y)
-#define mem(a,val) memset(a,val,sizeof(a))
-#define eb emplace_back
-#define f first
-#define s second
-
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	int a,b,c;
-	cin>>a>>b>>c;
-	int x=min(a,min(b,c));
-	if(a==x)
-	cout<<(3*a+2);
-	else if(x==b)
-	cout<<(3*a+1);
-	else
-	cout<<(3*a-3);
-	return 0;
+  int a,b,c;
+  cin >> a >> b >> c;
+  int x = min ( a , min ( b , c ) );
+  if ( x == a )
+  {
+      if( c < a + 2 )
+      {
+        x= c - 2 + c -1 +c;
+      }
+      else if(b < a + 1)
+      {
+        x = b-1 + b + b+1;
+      }
+      else
+      {
+        x = a + a + 1 + a + 2;
+      }
+  }
+  else if( x == b )
+  {
+    if ( c < b + 1 )
+    {
+      x= c - 2 + c - 1 + c;
+    }
+    else
+    {
+      x = b-1 + b + b + 1;
+    }
+  }
+  else
+  {
+    x= c - 2 + c - 1 + c;
+  }
+  cout << x;
 }
-
